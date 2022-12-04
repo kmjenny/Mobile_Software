9cee87e8a3c4ad99e8f2c4a68955b01d83f1b975
kmjenny <kjn3008@gmail.com>
2022-12-04 21:37:51 +0900

    DATE&TIME

--- app/src/main/java/com/course/finalproject/MainActivity2.java
+++ app/src/main/java/com/course/finalproject/MainActivity2.java
@@ -2,15 +2,30 @@ package com.course.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity2 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        int mHour = c.get(Calendar.HOUR_OF_DAY);
+        int mMinute = c.get(Calendar.MINUTE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
@@ -20,6 +35,41 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view){
                 Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
                 startActivity(intent);
+            }
+        });
+
+        TextView date = (TextView) findViewById(R.id.Date);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                date.setText(year + "-" + (month+1) + "-" + day);
+            }
+        },mYear,mMonth,mDay);
+
+        date.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(date.isClickable()){
+                    datePickerDialog.show();
+                }
+            }
+        });
+
+        TextView time = (TextView)findViewById(R.id.Time);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                time.setText(String.valueOf(i+":"+i1));
+            }
+        }, mHour, mMinute, false);
+
+
+        time.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(time.isClickable()){
+                    timePickerDialog.show();
+                }
             }
         });
     }
