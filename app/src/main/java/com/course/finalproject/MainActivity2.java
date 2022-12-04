46c68e0436ca9997370134947eca593643b0d289
kmjenny <kjn3008@gmail.com>
2022-12-04 17:34:44 +0900

    CONNECT BUTTON

--- app/src/main/java/com/course/finalproject/MainActivity2.java
+++ app/src/main/java/com/course/finalproject/MainActivity2.java
@@ -2,7 +2,10 @@ package com.course.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
