78d686eadaf7d680c4478834b62cf350c981c280
kmjenny <kjn3008@gmail.com>
2022-12-04 22:06:09 +0900

    INPUT IMAGE

--- app/src/main/java/com/course/finalproject/MainActivity2.java
+++ app/src/main/java/com/course/finalproject/MainActivity2.java
@@ -1,21 +1,35 @@
 package com.course.finalproject;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import com.bumptech.glide.Glide;
 
 import java.util.Calendar;
 
 public class MainActivity2 extends AppCompatActivity {
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Calendar c = Calendar.getInstance();
@@ -72,5 +86,35 @@ public class MainActivity2 extends AppCompatActivity {
                 }
             }
         });
+
+        imageView = (ImageView) findViewById(R.id.main_image);
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            launcher.launch(intent);
+        });
     }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>()
+            {
+                @Override
+                public void onActivityResult(ActivityResult result)
+                {
+                    if (result.getResultCode() == RESULT_OK)
+                    {
+                        Log.e(TAG, "result : " + result);
+                        Intent intent = result.getData();
+                        Log.e(TAG, "intent : " + intent);
+                        Uri uri = intent.getData();
+                        Log.e(TAG, "uri : " + uri);
+//                        imageview.setImageURI(uri);
+                        Glide.with(MainActivity2.this)
+                                .load(uri)
+                                .into(imageView);
+                    }
+                }
+            });
 }
+
