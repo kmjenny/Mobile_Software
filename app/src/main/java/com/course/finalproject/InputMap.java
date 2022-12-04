b636a15dbb588ec0d122ae79a9678eb551815dff
kmjenny <kjn3008@gmail.com>
2022-12-04 23:23:07 +0900

    MAP TO SEOUL

--- app/src/main/java/com/course/finalproject/InputMap.java
+++ app/src/main/java/com/course/finalproject/InputMap.java
@@ -3,12 +3,15 @@ package com.course.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class InputMap extends AppCompatActivity implements OnMapReadyCallback {
@@ -25,19 +28,69 @@ public class InputMap extends AppCompatActivity implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(final GoogleMap googleMap) {
-
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        oneMarker();
     }
+
+    public void oneMarker() {
+        // 서울 여의도에 대한 위치 설정
+        LatLng seoul = new LatLng(37.52487, 126.92723);
+
+        // 구글맵에 표시할 마커에 대한 옵션 설정
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions
+                .position(seoul)
+                .title("원하는 위치(위도, 경도)에 마커를 표시했습니다")
+                .snippet("여기는 여의도입니다")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .alpha(0.5f); // 알파는 좌표의 투명도
+
+        // 마커를 생성한다
+        mMap.addMarker(markerOptions); // .showInfoWindow() 를 쓰면 처음부터 마커에 상세정보가 뜬다
+
+        // 정보창 클릭리스너
+        mMap.setOnInfoWindowClickListener(infoWindowClickListener);
+
+        // 마커 클릭 리스너
+        mMap.setOnMarkerClickListener(markerClickListener);
+
+        // 카메라를 여의도 위치로 옮겨준다
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 16));
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(InputMap.this, "눌렀습니다!!", Toast.LENGTH_LONG);
+                return false;
+            }
+        });
+    }
+
+    // 정보창 클릭 리스너
+    GoogleMap.OnInfoWindowClickListener infoWindowClickListener = new GoogleMap.OnInfoWindowClickListener() {
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            String markerId = marker.getId();
+            Toast.makeText(InputMap.this, "정보창 클릭 Marker ID : "
+                    + markerId, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    // 마커 클릭 리스너
+    GoogleMap.OnMarkerClickListener markerClickListener = new GoogleMap.OnMarkerClickListener() {
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            String markerId = marker.getId();
+            // 선택한 타겟의 위치
+            LatLng location = marker.getPosition();
+            Toast.makeText(InputMap.this, "마커 클릭 Marker ID : "
+                            + markerId + "(" + location.latitude + " " + location.longitude + ")",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    };
 }
